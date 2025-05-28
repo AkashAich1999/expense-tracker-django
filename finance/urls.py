2272b1a9b215b9ab0de5edeb2c5578179482521c
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2025-05-28 13:20:06 +0530

    deleting home.html file & removing some lines from views.py & urls.py

--- finance/urls.py
+++ finance/urls.py
@@ -1,8 +1,5 @@
-from django.urls import path, include
-from finance.views import Home, HomeView
+from django.urls import path
 
 urlpatterns = [
-    path('', Home, name="home"),
-    path('home/', Home, name="home"),
-    path('ghar/', HomeView.as_view(), name="ghar"),
+    
 ]
