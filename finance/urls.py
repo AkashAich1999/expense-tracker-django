b864ff3b1fc7cb5274a9a3335c700c12fc35d8e2
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2025-05-28 20:12:38 +0530

    django built-in User Model & built-in Form (UserCreationForm)

--- finance/urls.py
+++ finance/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from finance.views import RegisterView
 
 urlpatterns = [
-    
+    path("register/", RegisterView.as_view(), name="register")
 ]
--- finance/views.py
+++ finance/views.py
@@ -1,2 +1,8 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 from django.views import View
+from finance.forms import RegisterForm
+
+class RegisterView(View):
+    def get(self, request, *args, **kwargs):
+        form = RegisterForm()
+        return render(request, 'finance/register.html', {'form':form})
