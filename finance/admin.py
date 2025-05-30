9b1f028761ab9bd6c6ab42161c104cf6fe7fbabc
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2025-05-30 21:42:08 +0530

    Goal added

--- finance/admin.py
+++ finance/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from finance.models import Transaction
+from finance.models import Transaction, Goal
 
 # Register your models here.
 admin.site.register(Transaction)
+admin.site.register(Goal)
