aeda650f5bb3b09512b932c76b4ca7a203230f5e
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2025-05-30 19:27:01 +0530

    TransactionCreateView

--- finance/models.py
+++ finance/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 
 class Transaction(models.Model):
     TRANSACTION_TYPES = [
-        ('Inc', 'Income'),
-        ('Exp', 'Expense')
+        ('Income', 'Income'),
+        ('Expense', 'Expense')
     ]
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
@@ -12,3 +12,6 @@ class Transaction(models.Model):
     transaction_type = models.CharField(max_length=10, choices=TRANSACTION_TYPES)
     date = models.DateField()
     category = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
--- finance/views.py
+++ finance/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from finance.forms import RegisterForm
+from finance.forms import RegisterForm, TransactionForm
 from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -19,4 +19,18 @@ class RegisterView(View):
     
 class DashboardView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'finance/dashboard.html')    
+        return render(request, 'finance/dashboard.html')
+        
+class TransactionCreateView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        form = TransactionForm()
+        return render(request, 'finance/transaction_form.html', {'form': form})
+    
+    def post(self, request, *args, **kwargs):
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            transaction = form.save(commit=False)
+            transaction.user = request.user
+            transaction.save()
+            return redirect('dashboard')
+        return render(request, 'finance/transaction_form.html', {'form':form})
